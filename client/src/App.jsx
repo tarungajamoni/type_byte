31af9012180f462e06415ce8b44e772f85b60ade
tarungajamoni <saitarun.goud1998@gmail.com>
2024-02-13 20:53:37 +0530

    Added Pages, Routes, Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import About from './pages/About';
+import Header from './components/Header';
+import Projects from './pages/Projects';
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>App</h1>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path='*' element={<h1>Not Found</h1>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+      <Route path='/signin' element={<SignIn/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/projects' element={<Projects/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 

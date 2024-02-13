a4323b56c7c085c0a6a11de452a23a22c294a83a
tarungajamoni <saitarun.goud1998@gmail.com>
2024-02-13 23:51:08 +0530

    Connect to Server, Database

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Mongo is Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, () => {
+  console.log("Server is running!");
+});
